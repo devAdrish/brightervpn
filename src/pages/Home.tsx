0a6fd1a591fde7ff97607a772b80948ddff98982
Adrish Shahid <dev.adrishs@gmail.com>
2024-10-17 22:56:26 +0500

    add deeplink test buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,34 @@
-const Home = () => <h1>Hello World</h1>;
+const Home = () => {
+  const handleOpenApp = () => {
+    const appUrl = "https://brightervpn-test.vercel.app/mobile-app";
+    const fallbackUrl = "https://play.google.com/store/apps/details?id=com.yourapp";
+
+    window.location.href = appUrl;
+    setTimeout(() => {
+      window.location.href = fallbackUrl;
+    }, 2000);
+  };
+
+  const handleOpenMyAccount = () => {
+    const appUrl = "https://brightervpn-test.vercel.app/mobile-app/account";
+    const fallbackUrl = "https://play.google.com/store/apps/details?id=com.yourapp";
+    window.location.href = appUrl;
+
+    setTimeout(() => {
+      window.location.href = fallbackUrl;
+    }, 3000);
+  };
+
+  return (
+    <div>
+      <h1 style={{ margin: 10 }}>Hello World</h1>
+
+      <button style={{ margin: 10 }} onClick={handleOpenApp}>
+        Open BrighterVPN App
+      </button>
+      <button onClick={handleOpenMyAccount}>Open BrighterVPN Account</button>
+    </div>
+  );
+};
 
 export default Home;
