3e21feffcfb2307e153cf259a95fa062f6db6d42
Adrish Shahid <dev.adrishs@gmail.com>
2024-10-18 17:57:29 +0500

    testing aasa

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,17 @@
 const Home = () => {
   const handleOpenApp = () => {
     const appUrl = "https://brightervpn-test.vercel.app/mobile-app";
-    const fallbackUrl = "https://play.google.com/store/apps/details?id=com.yourapp";
-
     window.location.href = appUrl;
-    setTimeout(() => {
-      window.location.href = fallbackUrl;
-    }, 2000);
   };
 
   const handleOpenMyAccount = () => {
     const appUrl = "https://brightervpn-test.vercel.app/mobile-app/account";
-    const fallbackUrl = "https://play.google.com/store/apps/details?id=com.yourapp";
     window.location.href = appUrl;
+  };
 
-    setTimeout(() => {
-      window.location.href = fallbackUrl;
-    }, 3000);
+  const handleOpeniOS = () => {
+    const appUrl = "https://brightervpn-test.vercel.app";
+    window.location.href = appUrl;
   };
 
   return (
@@ -26,7 +21,12 @@ const Home = () => {
       <button style={{ margin: 10 }} onClick={handleOpenApp}>
         Open BrighterVPN App
       </button>
-      <button onClick={handleOpenMyAccount}>Open BrighterVPN Account</button>
+      <button style={{ margin: 10 }} onClick={handleOpenMyAccount}>
+        Open BrighterVPN Account
+      </button>
+      <button style={{ margin: 10 }} onClick={handleOpeniOS}>
+        Open BrighterVPN iOS
+      </button>
     </div>
   );
 };
