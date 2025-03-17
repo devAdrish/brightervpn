cc69686b2e5fd360d31c4c3de64a0d1502cceac8
Adrish Shahid <dev.adrishs@gmail.com>
2025-03-18 01:26:20 +0500

    testing for android 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+
 const customSchemeAppUrl = "voyagerapp://";
 const customSchemeAppResetUrl = "voyagerapp://reset-password/1de43347-124b-4a8b-bd4b-3af564972d34";
 
 const Home = () => {
+  useEffect(() => {
+    window.location.href =
+      "intent://reset-password/1de43347-124b-4a8b-bd4b-3af564972d34#Intent;scheme=voyagerapp;package=io.accesswealth.voyager;end";
+  }, []);
+
   return (
     <div>
       <h1 style={{ margin: 10 }}>Hello World v2</h1>
